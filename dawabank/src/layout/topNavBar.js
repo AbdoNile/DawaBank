39edf8457be1f8eb812b67b72944cb23b69be49e
Abdo <abdo@axetay.co.uk>
2017-11-01 21:21:28 +0000

    add routing to navbar

--- dawabank/src/layout/topNavBar.js
+++ dawabank/src/layout/topNavBar.js
@@ -1,55 +1,55 @@
 import React from 'react';
+import { Button, Nav, Navbar, NavDropdown, DropdownButton , MenuItem, NavItem } from 'react-bootstrap';
+
+import { LinkContainer } from 'react-router-bootstrap';
 
 class TopNavBar extends React.Component {
+
   render() {
     return (
-      <header>
+      <header> 
             <div className="container">
                 <div className="logo">
                     <a href="index.html"><img src="images/logo.png" alt=""/></a>
                 </div>
-                
-                <ul className="nav">
-                    <li className="active"><a href="index.html">Dashboard</a></li>
-                    <li><a href="My_Offers.html">My Offers</a></li>
-                    <li><a href="Search_Results.html">Search</a></li>
-                </ul>
+                <Nav>
+                    <LinkContainer to="/Find">
+                        <NavItem>Search</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/offer">
+                        <NavItem>Create Offer</NavItem>
+                    </LinkContainer>
+                    <LinkContainer to="/MyOffers">
+                        <NavItem>My Offers</NavItem>
+                    </LinkContainer>
+                </Nav>
                 <div className="actions">
-                    <div className="dropdown profile">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                    <DropdownButton noCaret  className="dropdown profile" id="user_profile" 
+                    title={
                             <i className="mIcon">&#xf206;</i>
-                    </button>
-                    
-                        <ul className="dropdown-menu">
-                            <li><a className="dropdown-item" href="#">My Profile</a></li>
-                            <li><a className="dropdown-item" href="#">Logout</a></li>
-                        </ul> 
-                    </div>
+                          
+                    } >
+                        <LinkContainer to="/profile">
+                            <NavItem>My Profile</NavItem>
+                        </LinkContainer>
+                        <LinkContainer to="/Logout">
+                            <NavItem>LogOut</NavItem>
+                        </LinkContainer>
+                    </DropdownButton >
+                        
                     <div className="dropdown notifications">
-                        <button className="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true"
-                            aria-expanded="false">
+                        <button className="btn btn-secondary dropdown-toggle" type="button"
+                         id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" 
+                         aria-expanded="false">
                             <i className="mIcon">&#xf1fb;</i>
                             <span className="new">3</span>
-                    </button>
-                        <ul className="dropdown-menu">
-                            <li className="not_seen">
-                                <a href="Messages.html">
-                                    <span className="time">now</span>
-                                    <h4>Ehab Salem</h4>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                </a>
-                            </li>
-                            <li>
-                                <a href="Messages.html">
-                                    <span className="time"><span>2</span>Mins ago</span>
-                                    <h4>Ahmed Shaarawy</h4>
-                                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</p>
-                                </a>
-                            </li>
-                            <a href="Messages.html" className="see_all"><i className="mIcon">&#xf275;</i>See All</a>
-                        </ul>
+                         </button>
+                        
                     </div>
-                    <a href="My_Offers.html" className="btn btn-primary has_icon"><i className="mIcon">&#xf158;</i>New Offer</a>
+                    <LinkContainer to="/offer">
+                        <a className="btn btn-primary has_icon"><i className="mIcon">&#xf158;</i>New Offer</a>    
+                    </LinkContainer>
+                    
                 </div>
             </div>
         </header>
