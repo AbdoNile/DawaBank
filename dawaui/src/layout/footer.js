e1b2ae34899e4c65bf19265e19bde36f365926eb
Abdo <abdo@axetay.co.uk>
2018-10-14 21:05:56 +0100

    Rmeove confusing project

--- dawaui/src/layout/footer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-class Footer extends React.Component {
-  render() {
-    return <div> <footer>Copyright © 2017 Dawa Bank</footer></div>;
-  }
-}
-
-export default Footer;
