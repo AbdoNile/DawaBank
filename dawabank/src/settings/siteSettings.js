80a3f875afd18fc4b13f07f3224cce12b3150555
Abdo <aali_hassan@hotmail.com>
2018-10-31 01:01:55 +0000

    remove google settings

--- dawabank/src/settings/siteSettings.js
+++ dawabank/src/settings/siteSettings.js
@@ -1,7 +1,7 @@
 const SiteSettings = {
     map : {
         googleApiKey : "AIzaSyAJHcO77eg3mYJ4fGzOgrB_mIcRKxsut3k",
-        language : "ar",
+        language : "en",
         defaultZoom : 4,
         defaultCentre :  {
                 lat: 50,
@@ -14,9 +14,6 @@ const SiteSettings = {
         address : "http://localhost:5000/api/"
     },
     authentication : {
-        google : {
-            client_id : "539302904518-ja3mcnpg4vdubpn7sk3soe18n2pp08dj.apps.googleusercontent.com",
-        },
         cognito : {
             login_page : "https://minttest.auth.us-east-2.amazoncognito.com/login?response_type=token&client_id=5p973ntoftvhdut4skdrgucba3&redirect_uri=",
         }
