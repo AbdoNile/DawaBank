053192d6979a8597766883b18e050e5dfe93950d
Abdo <aali_hassan@hotmail.com>
2018-11-10 21:21:16 +0000

    ECS Instances registering successfully

--- infrastructure/ecs/main.tf
+++ infrastructure/ecs/main.tf
@@ -4,3 +4,7 @@ variable "appName" { }
 output "iam-role-for-ecsInstances-name" {
   value = "${aws_iam_role.ecsInstanceRole.name}"
 }
+
+output "cluster-name" {
+  value = "${aws_ecs_cluster.ecs-preprod.name}"
+}
--- infrastructure/launch-group-ecs-instances.tf
+++ infrastructure/launch-group-ecs-instances.tf
@@ -9,20 +9,26 @@ resource "aws_launch_configuration" "ecs-instances" {
   name_prefix          = "ecs-instance-"
   key_name             = "${var.appName}"
   iam_instance_profile = "${aws_iam_instance_profile.ecsInstanceProfile.arn}"
- 
+ user_data = <<EOF
+ <powershell>
+Initialize-ECSAgent -Cluster ${module.project-ecs-cluster.cluster-name} -EnableTaskIAMRole -LoggingDrivers '["json-file","awslogs"]'
+</powershell>
+ EOF
   security_groups = [
     "${aws_security_group.localtraffic.id}",
   ]
+  lifecycle { create_before_destroy = true }
 }
 
 resource "aws_autoscaling_group" "ecs-instances" {
   name                 = "ECS Instances Servers"
   max_size             = 2
   min_size             = 0
-  desired_capacity     = 1
+  desired_capacity     = 2
   placement_group      = "${aws_placement_group.spread.id}"
   launch_configuration = "${aws_launch_configuration.ecs-instances.id}"
   vpc_zone_identifier  = ["${module.network.private-subnet-1-id},${module.network.private-subnet-2-id}"]
+  lifecycle { create_before_destroy = true }
 }
 
 resource "aws_iam_instance_profile" "ecsInstanceProfile" {
