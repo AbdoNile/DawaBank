b36833d099a723d6dc113686166f7de441c17823
Abdo <aali_hassan@hotmail.com>
2018-12-02 22:03:40 +0000

    changes to google auth

--- infrastructure/Frontend/cognito.tf
+++ infrastructure/Frontend/cognito.tf
@@ -9,9 +9,10 @@ resource "aws_cognito_user_pool_client" "client" {
   name = "${var.appName}-${var.environment}"
   callback_urls = ["https://${aws_s3_bucket.web_bucket.website_endpoint}/login-call-back" , "http://localhost:3000/login-call-back"]
   user_pool_id = "${aws_cognito_user_pool.pool.id}"
-  allowed_oauth_flows = ["implicit"]
-  allowed_oauth_scopes = ["openid", "profile"]
+  allowed_oauth_flows = ["implicit" ]
+  allowed_oauth_scopes = ["openid", "profile" , "email"]
   supported_identity_providers = ["Google"]
+  allowed_oauth_flows_user_pool_client = true
 }
 
 resource "aws_cognito_identity_provider" "google" {
@@ -28,5 +29,10 @@ resource "aws_cognito_identity_provider" "google" {
   attribute_mapping {
     email    = "email"
     username = "sub"
+    family_name = "family_name"
+    given_name ="given_name"
+    picture = "picture"
+    birthdate = "birthdays"
+    gender = "genders"
   }
 }
