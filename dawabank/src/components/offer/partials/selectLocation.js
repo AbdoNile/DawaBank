d3e3bd38f671809e015c3c582e5c2ab88828ddf3
Abdo <aali_hassan@hotmail.com>
2019-03-24 16:29:41 +0000

    tidying up

--- dawabank/src/components/offer/partials/selectLocation.js
+++ dawabank/src/components/offer/partials/selectLocation.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import LocationPicker from 'generic/google/locationPicker';
+import LocationPicker from '../../../generic/google/locationPicker';
 import LabelWrapper from '../../../generic/bootstrap/controlWrapper/labelWrapper';
 import Validated from '../../../utility/Validation/validated'
 
